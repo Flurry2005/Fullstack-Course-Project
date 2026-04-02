22c53a1876ec9d88312969d879675b982f6e68e8
Ralleballebamse <raller@hotmail.se>
2026-04-02 19:43:49 +0200

    Start with checkoutPage

---
+++ src/Frontend/checkoutPage/checkoutPage.tsx
@@ -0,0 +1,13 @@
+// import InputField from "../Components/InputField";
+import "../App.css";
+// import GlowingButton from "../Components/GlowingButton";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+function CheckoutPage() {
+    return (
+        <div>Hello</div>
+    );
+}
+
+export default CheckoutPage;
