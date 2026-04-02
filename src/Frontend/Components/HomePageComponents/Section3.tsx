eb47532e724eabc476503b40b1ccc1e9389e8272
Driev <158128154+ItzDriev@users.noreply.github.com>
2026-04-03 00:40:55 +0200

    The hompage's third section now has a card design added to it and looks pretty good, design is similar to the cards of section2

--- src/Frontend/Components/HomePageComponents/Section3.tsx
+++ src/Frontend/Components/HomePageComponents/Section3.tsx
@@ -1,7 +1,23 @@
+import ServiceCard from "./ServiceCard";
+
 function Section2() {
   return (
-    <section className="z-0 bg-[#f9f5ff] w-full h-140">
-      <div>balls</div>
+    <section className="flex justify-between items-start gap-6 mb-10 px-10">
+      <div>
+        <h2 className="my-5 font-semibold text-[#23235b] text-[44px] leading-tight">
+          Popular Services
+        </h2>
+
+        <div className="gap-6 grid grid-cols-1 grid-cols-2 lg:grid-cols-4 sm:">
+          <ServiceCard
+            image={"/HomePage/goon.jpg"}
+            rating={"5"}
+            name={"Placeholder"}
+            description={"LOREM IPSUM DOLOR LIGMA BLYAT"}
+            price={"$67.69"}
+          />
+        </div>
+      </div>
     </section>
   );
 }
