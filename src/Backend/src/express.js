1d930a23c4ebd50afc73bc816a58afde2ea4a9c2
Flurry <41346632+Flurry2005@users.noreply.github.com>
2026-04-03 02:02:20 +0200

    Fixed cors?

--- src/Backend/src/express.js
+++ src/Backend/src/express.js
@@ -1,6 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 import { routerr as mainRouter } from "./routes/index.js";
 export const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "https://frontend-w1uy.onrender.com", // your frontend
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  }),
+);
 
 app.use(mainRouter);
