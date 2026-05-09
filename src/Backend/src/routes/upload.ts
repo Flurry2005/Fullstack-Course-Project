6f11dc0127bdfc7201da684a9e8f9e46c1549654
Flurry <41346632+Flurry2005@users.noreply.github.com>
2026-05-10 01:03:55 +0200

    Remove: Unused route

--- src/Backend/src/routes/upload.ts
+++ src/Backend/src/routes/upload.ts
@@ -2,6 +2,7 @@ import router from "express";
 import { upload } from "../../utils/multer";
 import { uploadBuffer, getSquareImage } from "../../services/Cloudinary";
 import userModel from "../models/userModel";
+import gigsModel from "../models/gigsModel";
 export const uploadRouter = router();
 
 uploadRouter.post(
@@ -19,41 +20,6 @@ uploadRouter.post(
         fileBuffer,
         res.locals.jwt.username + "-profilePicture",
         "profilePictures",
-      );
-
-      const url = getSquareImage(result.public_id, result.version);
-
-      await userModel.updateOne(
-        { _id: res.locals.jwt._id },
-        {
-          $set: {
-            profilePictureUrl: url,
-          },
-        },
-      );
-
-      res.send(url);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
-);
-
-uploadRouter.post(
-  "/upload/gigPicture",
-  upload.single("file"),
-  async (req, res) => {
-    try {
-      const fileBuffer = req.file?.buffer;
-
-      if (!fileBuffer) {
-        return res.status(400).json({ error: "No file uploaded" });
-      }
-
-      const result: any = await uploadBuffer(
-        fileBuffer,
-        res.locals.jwt.username + "-profilePicture",
-        "gigPreviewImages",
       );
 
       const url = getSquareImage(result.public_id, result.version);
