8faada9d0f643a6b011ec3c5e85bc60528066e14
ErikWin1337 <erikwin2@gmail.com>
2026-05-14 21:26:29 +0200

    fix(homePage): Made Homepage responsive

--- Frontend/HomePage/HomePageComponents/Section2Grid.tsx
+++ Frontend/HomePage/HomePageComponents/Section2Grid.tsx
@@ -2,7 +2,7 @@ import Card from "./Section2Card";
 
 function Section2Grid() {
   return (
-    <div className="gap-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 mt-12">
+    <div className="gap-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 mt-10 sm:mt-12">
       <Card title="Design" icon={<i className="fa-solid fa-palette"></i>} />
       <Card title="Development" icon={<i className="fa-solid fa-code"></i>} />
       <Card title="Video" icon={<i className="fa-solid fa-video"></i>} />
