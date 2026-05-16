ab83fa60ad2095dae8ab40562ebe2703f8751e10
johan <kronholmjohan@gmail.com>
2026-05-16 10:25:53 +0200

    added links to buyer and service in orders

--- Frontend/SellerDashboard/Components/OrderCard.tsx
+++ Frontend/SellerDashboard/Components/OrderCard.tsx
@@ -2,9 +2,8 @@ import { useAuth } from "../../Context/useAuth";
 import type { Gig } from "../../types/Gig";
 import type { Order } from "../../types/Order";
 import StatusBadge from "../Components/StatusBadge";
-import bellIcon from "../../assets/bell-icon.svg";
-import infoIcon from "../../assets/info-icon.svg";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 interface props {
   order: Order;
@@ -55,6 +54,7 @@ function OrderCard({ order, gig, profilePictures }: props) {
     
             <div className="flex justify-between flex-wrap gap-3">
               <div className="flex gap-3 w-fit ">
+                     <Link to={`/profile/${order.buyerUsername}`}>
               <img
                 className="rounded-full w-16 h-16"
                 src={
@@ -68,11 +68,16 @@ function OrderCard({ order, gig, profilePictures }: props) {
                     "https://res.cloudinary.com/dnpnpkqig/image/upload/c_fill,f_auto,g_auto,h_500,q_auto,w_500/v1778358513/default-profilePicture?_a=BAMAPqUs0&t=1778358700344";
                 }}
               ></img>
+              </Link>
               <div className="flex flex-col">
+                <Link to={`/profile/${order.buyerUsername}`}>
                 <span className="font-semibold text-[#2C2A51]">
                   {order.buyerUsername}
                 </span>
+                  </Link>
+                  <Link to={`/services/${gig?.category?.main_slug}/${gig?.category?.sub_slug}/${gig?._id}`}>
                 <span className="text-[#5A5781]">{order.gigname}</span>
+                </Link>
               </div>
               </div>
                        <StatusBadge status={true} />
@@ -83,7 +88,7 @@ function OrderCard({ order, gig, profilePictures }: props) {
         
      
       
-    
+
         <div className="flex flex-col gap-3 py-3 border-[#f0eef8] border-t">
           <div className="flex flex-col">
             <span className="text-[#5A5781] text-sm">Due in</span>
@@ -115,8 +120,11 @@ function OrderCard({ order, gig, profilePictures }: props) {
                   <span className="font-semibold text-[10px] uppercase leading-3">
                     Service
                   </span>
+                  
                   <span className="ml-auto">
+                     <Link to={`/services/${gig?.category?.main_slug}/${gig?.category?.sub_slug}/${gig?._id}`}>
                     {gig?.title || order.gigname}
+                    </Link>
                   </span>{" "}
                 </div>
 
