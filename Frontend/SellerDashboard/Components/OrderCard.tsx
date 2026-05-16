99c884efdc68c208aa7b8e4791b5893a23928ca3
johan <kronholmjohan@gmail.com>
2026-05-16 10:12:51 +0200

    fix: design in ordercard

--- Frontend/SellerDashboard/Components/OrderCard.tsx
+++ Frontend/SellerDashboard/Components/OrderCard.tsx
@@ -51,9 +51,10 @@ function OrderCard({ order, gig, profilePictures }: props) {
   return (
     <>
       <div className="flex flex-col gap-6 bg-white p-6 border-[#ACA8D7]/15 border-7 rounded-2xl w-full h-fit">
-        <div className="relative flex flex-wrap gap-2">
-          <div className="relative w-full">
-            <div className="flex gap-3">
+
+    
+            <div className="flex justify-between flex-wrap gap-3">
+              <div className="flex gap-3 w-fit ">
               <img
                 className="rounded-full w-16 h-16"
                 src={
@@ -73,32 +74,43 @@ function OrderCard({ order, gig, profilePictures }: props) {
                 </span>
                 <span className="text-[#5A5781]">{order.gigname}</span>
               </div>
-            </div>
+              </div>
+                       <StatusBadge status={true} />
+           
           </div>
-          <span className="top-0 right-0 absolute cursor-pointer">
-            <StatusBadge status={true} />
-          </span>
-        </div>
+  
+     
+        
+     
+      
+    
         <div className="flex flex-col gap-3 py-3 border-[#f0eef8] border-t">
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col">
             <span className="text-[#5A5781] text-sm">Due in</span>
             <div className="flex items-center">
               <span
-                className={`flex px-3 items-center gap-3 font-semibold text-xl ${dueDateLabel.includes("ago") ? "text-red-400" : ""}`}
+                className={`flex px-3 mx-auto gap-3 font-semibold text-xl ${dueDateLabel.includes("ago") ? "text-red-400" : ""}`}
               >
                 {dueDateLabel}
               </span>
             </div>
             <span
               onClick={() => setToggleInfo((prev) => !prev)}
-              className={`${toggleInfo ? "opacity-50" : ""} hover:opacity-50 text-[#3525CD]  text-2xl font-semibold cursor-pointer ml-auto w-7 h-7`}
+              className={`${toggleInfo ? "opacity-50" : ""} hover:opacity-50 text-[#ffffff] rounded-full shadow-md cursor-pointer ml-auto inline-grid place-items-center bg-linear-to-r from-[#4F46E5] to-[#4e46e5c2] border-[#b4b1b1] w-7 h-7 p-0`}
             >
-              [{toggleInfo ? "−" : "+"}]
+              {toggleInfo ? (
+                <span className="block w-3 h-0.5 bg-white rounded-full"></span>
+              ) : (
+                <span className="relative block w-3 h-3">
+                  <span className="top-1/2 left-0 absolute bg-white rounded-full w-3 h-0.5 -translate-y-1/2"></span>
+                  <span className="top-0 left-1/2 absolute bg-white rounded-full w-0.5 h-3 -translate-x-1/2"></span>
+                </span>
+              )}
             </span>
           </div>
           {toggleInfo && (
             <>
-              <div className="flex flex-col justify-between gap-3 bg-[#C3C0FF]/10 px-3 py-1 border-[#f0eef8] border-t text-[#3525CD]">
+              <div className="flex flex-col shadow-md justify-between gap-3 bg-[#C3C0FF]/10 px-3 py-1 border-[#f0eef8] border-t text-[#3525CD]">
                 <div className="flex flex-col">
                   <span className="font-semibold text-[10px] uppercase leading-3">
                     Service
